20c94aa2b1f922c9770660acef5c2f1db03f528d
ChloeCrozier <chloecrozier@gmail.com>
2024-09-30 21:33:00 -0400

    project euler: #1

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-**Total Solutions:** 6
+**Total Solutions:** 7
 
 - **Basic:**        6
 - **Intermediate:**        0
 - **Advanced:**        0
+- **Project Euler:**        1
 
--- projectEuler/0001_multiples_of_3_and_5.py
+++ projectEuler/0001_multiples_of_3_and_5.py
@@ -1,13 +1,11 @@
-#!/bin/python3
+k#!/bin/python3
 import sys
 
-t = int(input().strip())
+def sum(divisor, upperRange):
+    multiples = (upperRange - 1) // divisor
+    return divisor * ((multiples * (multiples + 1)) // 2)
 
+t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    
-    sum = 0
-    for i in range(0, n):
-        if i % 3 == 0 or i % 5 == 0:
-            sum += i
-    print(sum)
+    print(sum(3, n) + sum(5, n) - sum(15, n))
